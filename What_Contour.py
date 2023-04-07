c39c63114459a8ae7d9f9327758fa253dc293c68
simgeduru <79364942+simgeduru@users.noreply.github.com>
2023-04-07 13:25:07 +0300

    V-1.0
    by changing the threshold value you can find more or less edge

---
+++ What_Contour.py
@@ -0,0 +1,15 @@
+import numpy as np
+import cv2 as cv
+im = cv.imread('dersresim2.jpg')
+assert im is not None, "file could not be read, check with os.path.exists()"
+
+imgray = cv.cvtColor(im, cv.COLOR_BGR2GRAY)
+ret, thresh = cv.threshold(imgray, 80, 255, 0)
+
+contours, hierarchy = cv.findContours(imgray, cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE)
+
+cv.drawContours(im, contours, -1, (0,255,0), 3)
+cv.imshow("pencere",thresh)
+cv.imshow("pencere2",im)
+cv.waitKey(0)
+cv.destroyAllWindows()
